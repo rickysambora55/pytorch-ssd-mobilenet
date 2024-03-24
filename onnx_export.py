cda2ec09bfc7b6f172abd6b8f884af810df30914
Ricky Sambora <rickysambora55@gmail.com>
2024-03-24 15:20:30 +0700

    Base

--- onnx_export.py
+++ onnx_export.py
@@ -1,29 +1,25 @@
-#!/usr/bin/env python3
-# converts a saved PyTorch model to ONNX format
 import os
 import sys
 import argparse
 
 import torch.onnx
 
-from vision.ssd.vgg_ssd import create_vgg_ssd
 from vision.ssd.mobilenetv1_ssd import create_mobilenetv1_ssd
 from vision.ssd.mobilenetv1_ssd_lite import create_mobilenetv1_ssd_lite
-from vision.ssd.squeezenet_ssd_lite import create_squeezenet_ssd_lite
 from vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite
+from vision.ssd.mobilenet_v3_ssd_lite import create_mobilenetv3_ssd_lite
 
 from vision.ssd.config import mobilenetv1_ssd_config
 
 
 # parse command line
 parser = argparse.ArgumentParser()
-parser.add_argument('--net', default="ssd-mobilenet", help="The network architecture, it can be mb1-ssd (aka ssd-mobilenet), mb1-ssd-lite, mb2-ssd-lite or vgg16-ssd.")
+parser.add_argument('--net', default="mb1-ssd", help="The network architecture, it can be mb1-ssd, mb1-ssd-lite, mb2-ssd-lite or mb3-ssd-lite.")
 parser.add_argument('--input', type=str, default='', help="path to input PyTorch model (.pth checkpoint)")
 parser.add_argument('--output', type=str, default='', help="desired path of converted ONNX model (default: <NET>.onnx)")
 parser.add_argument('--labels', type=str, default='labels.txt', help="name of the class labels file")
-parser.add_argument('--resolution', type=int, default=300, help="the NxN pixel resolution of the model (can be changed for mb1-ssd only)")
 parser.add_argument('--batch-size', type=int, default=1, help="batch size of the model to be exported (default=1)")
-parser.add_argument('--model-dir', type=str, default='', help="directory to look for the input PyTorch model in, and export the converted ONNX model to (if --output doesn't specify a directory)")
+parser.add_argument('--model', type=str, default='', help="directory to look for the input PyTorch model in, and export the converted ONNX model to (if --output doesn't specify a directory)")
 
 args = parser.parse_args() 
 print(args)
@@ -70,19 +66,16 @@ num_classes = len(class_names)
 # construct the network architecture
 print(f"=> creating network:  {args.net}")
 print(f"=> num classes:       {num_classes}")
-print(f"=> resolution:        {args.resolution}x{args.resolution}")
 
-if args.net == 'vgg16-ssd':
-    net = create_vgg_ssd(len(class_names), is_test=True)
-elif args.net == 'mb1-ssd' or args.net == 'ssd-mobilenet':
-    mobilenetv1_ssd_config.set_image_size(args.resolution)
+if args.net == 'mb1-ssd':
+    mobilenetv1_ssd_config.set_image_size(300)
     net = create_mobilenetv1_ssd(len(class_names), is_test=True)
 elif args.net == 'mb1-ssd-lite':
     net = create_mobilenetv1_ssd_lite(len(class_names), is_test=True)
 elif args.net == 'mb2-ssd-lite':
     net = create_mobilenetv2_ssd_lite(len(class_names), is_test=True)
-elif args.net == 'sq-ssd-lite':
-    net = create_squeezenet_ssd_lite(len(class_names), is_test=True)
+elif args.net == 'mb3-ssd-lite':
+    net = create_mobilenetv2_ssd_lite(len(class_names), is_test=True)
 else:
     print("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
     sys.exit(1)
@@ -95,7 +88,7 @@ net.to(device)
 net.eval()
 
 # create example image data
-dummy_input = torch.randn(args.batch_size, 3, args.resolution, args.resolution).cuda()
+dummy_input = torch.randn(args.batch_size, 3, 300, 300).cuda()
 
 # format output model path
 if not args.output:
--- train_ssd.py
+++ train_ssd.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python3
-#
-# train an SSD detection model on Pascal VOC or Open Images datasets
-# https://github.com/dusty-nv/jetson-inference/blob/master/docs/pytorch-ssd.md
-#
 import os
 import sys
 import logging
@@ -13,116 +8,78 @@ import torch
 
 from torch.utils.data import DataLoader, ConcatDataset
 from torch.utils.tensorboard import SummaryWriter
-from torch.optim.lr_scheduler import CosineAnnealingLR, MultiStepLR
+from torch.optim.lr_scheduler import CosineAnnealingLR
 
-from vision.utils.misc import Timer, freeze_net_layers, store_labels
 from vision.ssd.ssd import MatchPrior
-from vision.ssd.vgg_ssd import create_vgg_ssd
 from vision.ssd.mobilenetv1_ssd import create_mobilenetv1_ssd
 from vision.ssd.mobilenetv1_ssd_lite import create_mobilenetv1_ssd_lite
 from vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite
-from vision.ssd.squeezenet_ssd_lite import create_squeezenet_ssd_lite
-from vision.datasets.voc_dataset import VOCDataset
-from vision.datasets.open_images import OpenImagesDataset
-from vision.nn.multibox_loss import MultiboxLoss
-from vision.ssd.config import vgg_ssd_config
+from vision.ssd.mobilenet_v3_ssd_lite import create_mobilenetv3_ssd_lite
 from vision.ssd.config import mobilenetv1_ssd_config
-from vision.ssd.config import squeezenet_ssd_config
 from vision.ssd.data_preprocessing import TrainAugmentation, TestTransform
 
+from vision.datasets.voc_dataset import VOCDataset
+from vision.nn.multibox_loss import MultiboxLoss
+from vision.utils.misc import Timer, freeze_net_layers, store_labels
+
 from eval_ssd import MeanAPEvaluator
-
-
-DEFAULT_PRETRAINED_MODEL='models/mobilenet-v1-ssd-mp-0_675.pth'
 
 
 parser = argparse.ArgumentParser(
     description='Single Shot MultiBox Detector Training With PyTorch')
 
 # Params for datasets
-parser.add_argument("--dataset-type", default="open_images", type=str,
-                    help='Specify dataset type. Currently supports voc and open_images.')
 parser.add_argument('--datasets', '--data', nargs='+', default=["data"], help='Dataset directory path')
-parser.add_argument('--balance-data', action='store_true',
-                    help="Balance training data by down-sampling more frequent labels.")
 
 # Params for network
 parser.add_argument('--net', default="mb1-ssd",
-                    help="The network architecture, it can be mb1-ssd, mb1-ssd-lite, mb2-ssd-lite or vgg16-ssd.")
-parser.add_argument('--resolution', type=int, default=300,
-                    help="the NxN pixel resolution of the model (can be changed for mb1-ssd only)")
-parser.add_argument('--freeze-base-net', action='store_true',
+                    help="The network architecture, it can be mb1-ssd, mb1-ssd-lite, mb2-ssd-lite or mb3-ssd-lite.")
+parser.add_argument('--freeze-base-net', default=True, action='store_true',
                     help="Freeze base net layers.")
-parser.add_argument('--freeze-net', action='store_true',
+parser.add_argument('--freeze-net', default=False, action='store_true',
                     help="Freeze all the layers except the prediction head.")
-parser.add_argument('--mb2-width-mult', default=1.0, type=float,
-                    help='Width Multiplifier for MobilenetV2')
 
 # Params for loading pretrained basenet or checkpoints.
-parser.add_argument('--base-net', help='Pretrained base model')
-parser.add_argument('--pretrained-ssd', default=DEFAULT_PRETRAINED_MODEL, type=str, help='Pre-trained base model')
 parser.add_argument('--resume', default=None, type=str, help='Checkpoint state_dict file to resume training from')
 
-# Params for SGD
+# Params for ASGD
 parser.add_argument('--lr', '--learning-rate', default=0.01, type=float,
                     help='initial learning rate')
-parser.add_argument('--momentum', default=0.9, type=float,
-                    help='Momentum value for optim')
 parser.add_argument('--weight-decay', default=5e-4, type=float,
                     help='Weight decay for SGD')
-parser.add_argument('--gamma', default=0.1, type=float,
-                    help='Gamma update for SGD')
-parser.add_argument('--base-net-lr', default=0.001, type=float,
-                    help='initial learning rate for base net, or None to use --lr')
-parser.add_argument('--extra-layers-lr', default=None, type=float,
-                    help='initial learning rate for the layers not in base net and prediction heads.')
-
-# Scheduler
-parser.add_argument('--scheduler', default="cosine", type=str,
-                    help="Scheduler for SGD. It can one of multi-step and cosine")
-
-# Params for Multi-step Scheduler
-parser.add_argument('--milestones', default="80,100", type=str,
-                    help="milestones for MultiStepLR")
-
-# Params for Cosine Annealing
-parser.add_argument('--t-max', default=100, type=float,
-                    help='T_max value for Cosine Annealing Scheduler.')
 
 # Train params
-parser.add_argument('--batch-size', default=4, type=int,
+parser.add_argument('--batch-size', '--batch', default=4, type=int,
                     help='Batch size for training')
-parser.add_argument('--num-epochs', '--epochs', default=30, type=int,
+parser.add_argument('--num-epochs', '--epoch', default=30, type=int,
                     help='the number epochs')
 parser.add_argument('--num-workers', '--workers', default=2, type=int,
                     help='Number of workers used in dataloading')
-parser.add_argument('--validation-epochs', default=1, type=int,
-                    help='the number epochs between running validation')
-parser.add_argument('--validation-mean-ap', action='store_true',
+parser.add_argument('--validation-mean-ap', default=True, action='store_true',
                     help='Perform computation of Mean Average Precision (mAP) during validation')
 parser.add_argument('--debug-steps', default=10, type=int,
                     help='Set the debug log output frequency.')
 parser.add_argument('--use-cuda', default=True, action='store_true',
                     help='Use CUDA to train model')
-parser.add_argument('--checkpoint-folder', '--model-dir', default='models/',
+parser.add_argument('--checkpoint-folder', '--model', default='models/',
                     help='Directory for saving checkpoint models')
-parser.add_argument('--log-level', default='info', type=str,
-                    help='Logging level, one of:  debug, info, warning, error, critical (default: info)')
                                         
 args = parser.parse_args()
 
-logging.basicConfig(stream=sys.stdout, level=getattr(logging, args.log_level.upper(), logging.INFO),
+# Set logging
+logging.basicConfig(stream=sys.stdout, level=getattr(logging, 'INFO', logging.INFO),
                     format='%(asctime)s - %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
-                    
-tensorboard = SummaryWriter(log_dir=os.path.join(args.checkpoint_folder, "tensorboard", f"{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}"))
 
+# Tensorboard                    
+tensorboard = SummaryWriter(log_dir=os.path.join(args.checkpoint_folder, "tensorboard"))
+
+# CPU or GPU training
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() and args.use_cuda else "cpu")
-
 if args.use_cuda and torch.cuda.is_available():
     torch.backends.cudnn.benchmark = True
     logging.info("Using CUDA...")
 
-
+# Training function
 def train(loader, net, criterion, optimizer, device, debug_steps=100, epoch=-1):
     net.train(True)
     
@@ -218,66 +175,53 @@ if __name__ == '__main__':
 
     logging.info(args)
     
-    # make sure that the checkpoint output dir exists
+    # Make sure that the checkpoint output dir exists
     if args.checkpoint_folder:
         args.checkpoint_folder = os.path.expanduser(args.checkpoint_folder)
-
         if not os.path.exists(args.checkpoint_folder):
             os.mkdir(args.checkpoint_folder)
             
-    # select the network architecture and config     
-    if args.net == 'vgg16-ssd':
-        create_net = create_vgg_ssd
-        config = vgg_ssd_config
-    elif args.net == 'mb1-ssd':
+    # Select the network architecture and config
+    if args.net == 'mb1-ssd':
         create_net = create_mobilenetv1_ssd
         config = mobilenetv1_ssd_config
-        config.set_image_size(args.resolution)
+        pretrained_ssd = 'pretrained/mobilenet-v1-ssd-mp-0_675.pth'
+        config.set_image_size(30)
     elif args.net == 'mb1-ssd-lite':
         create_net = create_mobilenetv1_ssd_lite
         config = mobilenetv1_ssd_config
-    elif args.net == 'sq-ssd-lite':
-        create_net = create_squeezenet_ssd_lite
-        config = squeezenet_ssd_config
+        pretrained_ssd = 'pretrained/mobilenet-v1-ssd-mp-0_675.pth'
     elif args.net == 'mb2-ssd-lite':
-        create_net = lambda num: create_mobilenetv2_ssd_lite(num, width_mult=args.mb2_width_mult)
+        create_net = lambda num: create_mobilenetv2_ssd_lite(num, 1)
         config = mobilenetv1_ssd_config
+        pretrained_ssd = 'pretrained/mb2-ssd-lite-mp-0_686.pth'
+    elif args.net == 'mb3-ssd-lite':
+        create_net = lambda num: create_mobilenetv3_ssd_lite(num)
+        config = mobilenetv1_ssd_config
+        pretrained_ssd = 'pretrained/ssdlite320_mobilenet_v3_large_coco-a79551df.pth'
     else:
         logging.fatal("The net type is wrong.")
         parser.print_help(sys.stderr)
         sys.exit(1)
         
-    # create data transforms for train/test/val
+    # Create data transforms for train/test/val
     train_transform = TrainAugmentation(config.image_size, config.image_mean, config.image_std)
     target_transform = MatchPrior(config.priors, config.center_variance,
                                   config.size_variance, 0.5)
-
     test_transform = TestTransform(config.image_size, config.image_mean, config.image_std)
 
-    # load datasets (could be multiple)
+    # Load datasets (could be multiple)
     logging.info("Prepare training datasets.")
     datasets = []
     for dataset_path in args.datasets:
-        if args.dataset_type == 'voc':
-            dataset = VOCDataset(dataset_path, transform=train_transform,
+        dataset = VOCDataset(dataset_path, transform=train_transform,
                                  target_transform=target_transform)
-            label_file = os.path.join(args.checkpoint_folder, "labels.txt")
-            store_labels(label_file, dataset.class_names)
-            num_classes = len(dataset.class_names)
-        elif args.dataset_type == 'open_images':
-            dataset = OpenImagesDataset(dataset_path,
-                 transform=train_transform, target_transform=target_transform,
-                 dataset_type="train", balance_data=args.balance_data)
-            label_file = os.path.join(args.checkpoint_folder, "labels.txt")
-            store_labels(label_file, dataset.class_names)
-            logging.info(dataset)
-            num_classes = len(dataset.class_names)
-
-        else:
-            raise ValueError(f"Dataset type {args.dataset_type} is not supported.")
+        label_file = os.path.join(checkpoint_folder, "labels.txt")
+        store_labels(label_file, dataset.class_names)
+        num_classes = len(dataset.class_names)
         datasets.append(dataset)
         
-    # create training dataset
+    # Create training dataset
     logging.info(f"Stored labels into file {label_file}.")
     train_dataset = ConcatDataset(datasets)
     logging.info("Train dataset size: {}".format(len(train_dataset)))
@@ -285,39 +229,29 @@ if __name__ == '__main__':
                               num_workers=args.num_workers,
                               shuffle=True)
                            
-    # create validation dataset                           
+    # Create validation dataset                           
     logging.info("Prepare Validation datasets.")
-    if args.dataset_type == "voc":
-        val_dataset = VOCDataset(dataset_path, transform=test_transform,
+    val_dataset = VOCDataset(dataset_path, transform=test_transform,
                                  target_transform=target_transform, is_test=True)
-    elif args.dataset_type == 'open_images':
-        val_dataset = OpenImagesDataset(dataset_path,
-                                        transform=test_transform, target_transform=target_transform,
-                                        dataset_type="test")
-        logging.info(val_dataset)
     logging.info("Validation dataset size: {}".format(len(val_dataset)))
-
-    val_loader = DataLoader(val_dataset, args.batch_size,
-                            num_workers=args.num_workers,
+    val_loader = DataLoader(val_dataset, batch_size,
+                            num_workers=num_workers,
                             shuffle=False)
                       
-    # create the network
+    # Create the network
     logging.info("Build network.")
     net = create_net(num_classes)
     min_loss = -10000.0
     last_epoch = -1
 
-    # prepare eval dataset (for mAP computation)
-    if args.validation_mean_ap:
-        if args.dataset_type == "voc":
-            eval_dataset = VOCDataset(dataset_path, is_test=True)
-        elif args.dataset_type == 'open_images':
-            eval_dataset = OpenImagesDataset(dataset_path, dataset_type="test")
-        eval = MeanAPEvaluator(eval_dataset, net, arch=args.net, eval_dir=os.path.join(args.checkpoint_folder, 'eval_results'))
+    # Prepare eval dataset (for mAP computation)
+    if validation_mean_ap:
+        eval_dataset = VOCDataset(dataset_path, is_test=True)
+        eval = MeanAPEvaluator(eval_dataset, net, arch=net_arch, eval_dir=os.path.join(checkpoint_folder, 'eval_results'))
         
-    # freeze certain layers (if requested)
-    base_net_lr = args.base_net_lr if args.base_net_lr is not None else args.lr
-    extra_layers_lr = args.extra_layers_lr if args.extra_layers_lr is not None else args.lr
+    # Freeze certain layers (if requested)
+    base_net_lr = lr
+    extra_layers_lr = lr
     
     if args.freeze_base_net:
         logging.info("Freeze base net.")
@@ -353,60 +287,47 @@ if __name__ == '__main__':
             )}
         ]
 
-    # load a previous model checkpoint (if requested)
+    # Load a previous model checkpoint (if requested)
     timer.start("Load Model")
     
-    if args.resume:
-        logging.info(f"Resuming from the model {args.resume}")
-        net.load(args.resume)
-    elif args.base_net:
-        logging.info(f"Init from base net {args.base_net}")
-        net.init_from_base_net(args.base_net)
-    elif args.pretrained_ssd:
-        logging.info(f"Init from pretrained SSD {args.pretrained_ssd}")
+    if resume:
+        logging.info(f"Resuming from the model {resume}")
+        net.load(resume)
+    elif pretrained_ssd:
+        logging.info(f"Init from pretrained SSD {pretrained_ssd}")
+        net.init_from_pretrained_ssd(pretrained_ssd)
         
-        if not os.path.exists(args.pretrained_ssd) and args.pretrained_ssd == DEFAULT_PRETRAINED_MODEL:
-            os.system(f"wget --quiet --show-progress --progress=bar:force:noscroll --no-check-certificate https://nvidia.box.com/shared/static/djf5w54rjvpqocsiztzaandq1m3avr7c.pth -O {DEFAULT_PRETRAINED_MODEL}")
+        if not os.path.exists(pretrained_ssd): 
+            logging.fatal("The net is wrong.")
+            parser.print_help(sys.stderr)
+            sys.exit(1)
 
-        net.init_from_pretrained_ssd(args.pretrained_ssd)
+        net.init_from_pretrained_ssd(pretrained_ssd)
         
     logging.info(f'Took {timer.end("Load Model"):.2f} seconds to load the model.')
 
-    # move the model to GPU
+    # Move the model to GPU
     net.to(DEVICE)
 
-    # define loss function and optimizer
+    # Define loss function and optimizer
     criterion = MultiboxLoss(config.priors, iou_threshold=0.5, neg_pos_ratio=3,
                              center_variance=0.1, size_variance=0.2, device=DEVICE)
-                             
-    optimizer = torch.optim.SGD(params, lr=args.lr, momentum=args.momentum,
-                                weight_decay=args.weight_decay)
+    optimizer = torch.optim.ASGD(params, lr=lr, weight_decay=args.weight_decay)
                                 
-    logging.info(f"Learning rate: {args.lr}, Base net learning rate: {base_net_lr}, "
-                 + f"Extra Layers learning rate: {extra_layers_lr}.")
+    logging.info(f"Learning rate: {args.lr}")
 
-    # set learning rate policy
-    if args.scheduler == 'multi-step':
-        logging.info("Uses MultiStepLR scheduler.")
-        milestones = [int(v.strip()) for v in args.milestones.split(",")]
-        scheduler = MultiStepLR(optimizer, milestones=milestones,
-                                                     gamma=0.1, last_epoch=last_epoch)
-    elif args.scheduler == 'cosine':
-        logging.info("Uses CosineAnnealingLR scheduler.")
-        scheduler = CosineAnnealingLR(optimizer, args.t_max, last_epoch=last_epoch)
-    else:
-        logging.fatal(f"Unsupported Scheduler: {args.scheduler}.")
-        parser.print_help(sys.stderr)
-        sys.exit(1)
+    # Set learning rate policy
+    logging.info("Uses CosineAnnealingLR scheduler.")
+    scheduler = CosineAnnealingLR(optimizer, num_epochs, last_epoch=last_epoch)
 
-    # train for the desired number of epochs
+    # Train for the desired number of epochs
     logging.info(f"Start training from epoch {last_epoch + 1}.")
     
     for epoch in range(last_epoch + 1, args.num_epochs):
         train(train_loader, net, criterion, optimizer, device=DEVICE, debug_steps=args.debug_steps, epoch=epoch)
         scheduler.step()
         
-        if epoch % args.validation_epochs == 0 or epoch == args.num_epochs - 1:
+        if epoch % 1 == 0 or epoch == args.num_epochs - 1:
             val_loss, val_regression_loss, val_classification_loss = test(val_loader, net, criterion, DEVICE)
             
             logging.info(
